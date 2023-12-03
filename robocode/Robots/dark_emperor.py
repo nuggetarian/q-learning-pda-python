e774f726ba121d80661298c483856e030d130464
nuggetarian <michal.zernovic@gmail.com>
2023-12-03 17:15:04 +0100

    Finally Done

--- robocode/Robots/dark_emperor.py
+++ robocode/Robots/dark_emperor.py
@@ -7,7 +7,7 @@ from Objects.robot import Robot
 FIRE_DISTANCE = 1000
 BULLET_POWER = 2
 
-class DarkEmperor(Robot): #Create a Robot
+class DarkEmperor(Robot):
 
     alpha = 0.1
     gamma = 0.6
@@ -18,20 +18,17 @@ class DarkEmperor(Robot): #Create a Robot
     last_state = 0
     reward = 100
     state = 6
-
-    # Initialize the Q-table with zeros
     q_table = np.zeros((num_states, num_actions))
 
-    def init(self):# NECESARY FOR THE GAME   To initialyse your robot
-        #Set the bot color in RGB
-        self.setColor(0, 200, 100)
+    def init(self):
+        self.setColor(72,209,204)
         self.setGunColor(200, 200, 0)
         self.setRadarColor(255, 60, 0)
         self.setBulletsColor(0, 200, 100)
         
-        #get the map size
-        size = self.getMapSize() #get the map size
-        self.radarVisible(True) # show the radarField
+
+        size = self.getMapSize()
+        self.radarVisible(True)
 
         self.lockRadar("gun")
         self.setRadarField("thin")
@@ -45,21 +42,13 @@ class DarkEmperor(Robot): #Create a Robot
         state = self.state
         action = self.selectAction(state)
         self.performAction(action)
-
-        print(state)
-        print(action)
-
-        # # TODO
-        # reward = self.getReward(state, action)  
-        print("Reward:" + str(self.reward))     
         self.updateQValue(state, action, self.reward)
 
         self.ctr = self.ctr + 1
         if self.ctr % 2 == 0:
-            print("Saving QTable")
             self.saveQTable()
 
-    def sensors(self):  #NECESARY FOR THE GAME
+    def sensors(self):
         pass
 
     def onHitWall(self):
@@ -69,7 +58,6 @@ class DarkEmperor(Robot): #Create a Robot
             self.reward = self.reward - 0
         
         self.state = 0
-        # self.move(-20)
         self.move(-random.randrange(10,20))
 
     def sensors(self): 
@@ -158,18 +146,11 @@ class DarkEmperor(Robot): #Create a Robot
         if dist < FIRE_DISTANCE:
             self.fire(BULLET_POWER) 
 
-    # TODO
-    def getState(self):
-        return 1
 
-    # TODO sposobuje error.
     def updateQValue(self, state, action, reward):
-        # print("USING OLD VALUE")
-        # print("This is the last state:" + str(self.last_state) + "\nThis is the action:" + str(action))
         old_value = self.q_table[self.last_state][action]
         next_max = np.max(self.q_table[state][action])
         
-        # new_value = ((1 - self.alpha) * old_value + self.alpha * (reward + self.gamma * next_max))/100
         new_value = (1 - self.alpha) * old_value + self.alpha * (reward + self.gamma * next_max)
         
         if new_value < 10:
@@ -181,11 +162,6 @@ class DarkEmperor(Robot): #Create a Robot
         
         self.q_table[state][action] = new_value
 
-        # old_value = self.q_table[0][1]
-        # next_max = np.max(self.q_table[0][1])
-        
-        # new_value = (1 - self.alpha) * old_value + self.alpha * (reward + self.gamma * next_max)
-        # self.q_table[0][1] = new_value
 
     def performAction(self, action):
         if action == 0:
@@ -213,13 +189,4 @@ class DarkEmperor(Robot): #Create a Robot
                 file.write(row_data + '\n')
     
     def loadQTable(self):
-        # loaded_array = np.zeros((self.num_states, self.num_actions))
-        # with open('qtable.txt', 'r') as file:
-        #     for line in file:
-        #         row = np.array([float(element) if '.' in element else int(element) for element in line.strip().split()])
-        #         self.q_table = np.vstack((self.q_table, row))
-
         self.q_table = np.loadtxt('qtable.txt', dtype=float)
-        
-        # self.q_table = loaded_array
-        print(self.q_table)
